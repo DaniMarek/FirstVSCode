5832f12debbc1f7f1e2ba766a8c81198a9de0b2a
Dani Marek <32878738+DaniMarek@users.noreply.github.com>
2018-12-21 21:24:49 -0600

    Add project files.

---
+++ Hello_World/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            var username = Console.ReadLine();
+            Console.WriteLine($"\nHello {username}!");
+            DateTime now = GetNow();
+            int dayOfYear = now.DayOfYear;
+
+            Console.Write("Day of year: ");
+            Console.WriteLine(dayOfYear);
+        }
+
+        private static DateTime GetNow()
+        {
+            return DateTime.Now;
+        }
+    }
+}
